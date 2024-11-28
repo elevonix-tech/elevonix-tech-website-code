bde0ec818120f0c8b59f14b51993fcd334f08537
bilal36529 <muhammadbilal36529@gmail.com>
2024-11-28 19:42:31 +0500

    home page sections added

---
+++ src/Components/Ourservices/OurServices.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import "./OurServices.css"
+
+const OurServices = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-12'>
+                    <div className='pt-5 mt-lg-5'>
+                        <h2 className='power_growth_h2'><span className='dark_brown_color'>We Provide</span> Services.</h2>
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+            <section class="g-our-services">
+                <div class="wrap-v4">
+                    <div class="items our-services-items-v5">
+                        <div class="item">
+                            <div class="item__content">
+                                <div class="left">
+                                    <h4>Team Augmentation</h4>
+                                    <h5>We provide engineers or teams tailored to your project, ensuring skill and compatibility with your goals and culture. They will collaborate with you closely, infusing fresh energy and ideas.</h5>
+                                    <div class="specs">
+                                        <div class="specs__item">Cultural fit</div>
+                                        <div class="specs__item"> Top 1%</div>
+                                        <div class="specs__item"> Instant hire</div>
+                                    </div>
+                                    <div class="actions-wrap">
+                                        <button class="action-btn -arrow-right" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                                        Let’s chat
+                                    </button>
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <img decoding="async" src="https://diffco.us/wp-content/uploads/2024/08/s-3.svg" title="1"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item__content">
+                                <div class="left">
+                                    <h4>Mobile Development</h4>
+                                    <h5>We specialize in developing native and cross-platform mobile applications for iOS and Android.</h5>
+                                    <div class="specs">
+                                        <div class="specs__item">Swift</div>
+                                        <div class="specs__item"> React Native</div>
+                                        <div class="specs__item"> Flutter</div>
+                                        <div class="specs__item"> Java</div>
+                                        <div class="specs__item"> Kotlin</div>
+                                    </div>
+                                    <div class="actions-wrap">
+                                        <button class="action-btn -arrow-right" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                                        Let’s chat
+                                    </button>
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <img decoding="async" src="https://diffco.us/wp-content/uploads/2024/08/s-1.svg" title="1"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item__content">
+                                <div class="left">
+                                    <h4>Back-end Development</h4>
+                                    <h5>We are experienced in high-load and complex backend infrastructure development for mobile or web apps and enterprise services.</h5>
+                                    <div class="specs">
+                                        <div class="specs__item">Node.js</div>
+                                        <div class="specs__item"> Go</div>
+                                        <div class="specs__item"> Python</div>
+                                        <div class="specs__item"> C#</div>
+                                        <div class="specs__item"> Java</div>
+                                    </div>
+                                    <div class="actions-wrap">
+                                        <button class="action-btn -arrow-right" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                                        Let’s chat
+                                    </button>
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <img decoding="async" src="https://diffco.us/wp-content/uploads/2024/08/s-2.svg" title="1"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item__content">
+                                <div class="left">
+                                    <h4>AI Development</h4>
+                                    <h5>Our team is experienced in Al, data analysis, machine learning, and implementing cutting-edge technologies.</h5>
+                                    <div class="specs">
+                                        <div class="specs__item">LLM</div>
+                                        <div class="specs__item"> Tensorflow</div>
+                                        <div class="specs__item"> Python</div>
+                                        <div class="specs__item"> PyTorch</div>
+                                        <div class="specs__item"> Computer Vision</div>
+                                    </div>
+                                    <div class="actions-wrap">
+                                        <button class="action-btn -arrow-right" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                                        Let’s chat
+                                    </button>
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <img decoding="async" src="https://diffco.us/wp-content/uploads/2024/08/s-4.svg" title="1"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item__content">
+                                <div class="left">
+                                    <h4>Front-end development</h4>
+                                    <h5>We are experienced in building modular, high-performance web applications for corporate clients and startups. We utilize modern and robust technology stacks.</h5>
+                                    <div class="specs">
+                                        <div class="specs__item">React</div>
+                                        <div class="specs__item"> Angular</div>
+                                        <div class="specs__item"> Electron</div>
+                                        <div class="specs__item"> Next.js</div>
+                                    </div>
+                                    <div class="actions-wrap">
+                                        <button class="action-btn -arrow-right" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                                        Let’s chat
+                                    </button>
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <img decoding="async" src="https://diffco.us/wp-content/uploads/2024/08/s-5.webp" title="1"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="item__content">
+                                <div class="left">
+                                    <h4>Web Development</h4>
+                                    <h5>Our developers can create web solutions that are tailored to your needs, easy to manage with popular CMS, and which can be seamlessly integrated with your existing internal systems.</h5>
+                                    <div class="specs">
+                                        <div class="specs__item">Contentful</div>
+                                        <div class="specs__item"> Magento</div>
+                                        <div class="specs__item"> WP</div>
+                                        <div class="specs__item"> Shopify</div>
+                                    </div>
+                                    <div class="actions-wrap">
+                                        <button class="action-btn -arrow-right" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                                        Let’s chat
+                                    </button>
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <img decoding="async" src="https://diffco.us/wp-content/uploads/2024/08/s-6.svg" title="1"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="actions">
+                        <a class="action-btn -gradient -arrow-right -size-xl" data-target="toggleBlock" data-type="popup" href="#modalGetQuote">
+                        Request a quote                    </a>
+                    </div>
+                </div>
+            </section>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OurServices
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavbarTop from '../Components/Navbar/Navbar'
 import Hero from '../Components/Hero/Hero'
+import ProvenProcess from '../Components/ProvenProcess/ProvenProcess'
+import Ourskills from '../Components/Ourskills/Ourskills'
+import Wegrowth from '../Components/Ourservices/OurServices'
 
 const Home = () => {
   return (
     <div>
       <NavbarTop />
       <Hero/>
+      <ProvenProcess/>
+      <Ourskills/>
+      <Wegrowth/>
     </div>
   )
 }
