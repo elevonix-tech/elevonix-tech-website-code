b7e48a60a0f3799f0e14d7e58c67f851316b4f04
bilal36529 <muhammadbilal36529@gmail.com>
2024-12-03 18:17:14 +0500

    added testimonials

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,36 +1,66 @@
-import React from 'react'
-import "./Hero.css"
+import React, { useEffect, useState } from 'react';
+import "./Hero.css";
+
 const Hero = () => {
+  const word = "ebsites."; // Word to animate
+  const [index, setIndex] = useState(0);
+  const [forward, setForward] = useState(true); // Direction flag
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => {
+        if (forward) {
+          if (prevIndex + 1 === word.length) {
+            setForward(false); // Reverse direction at the end
+            return prevIndex;
+          }
+          return prevIndex + 1;
+        } else {
+          if (prevIndex === 0) {
+            setForward(true); // Forward direction at the start
+            return prevIndex;
+          }
+          return prevIndex - 1;
+        }
+      });
+    }, 500); // Change letter every 500ms (adjust as needed)
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [forward, word.length]);
+
   return (
     <div className=''>
-      <main class="landing">
-        <img class="linesbg linesbg--landing mt-5 pt-5" src="https://truemarket.ca/wp-content/themes/truemarket/assets/dist/images/bg-asset-lines-01.svg" alt=""/>
-        <img class="pillbg pillbg--landing" src="https://truemarket.ca/wp-content/themes/truemarket/assets/dist/images/bg-asset-pill-right.svg" alt=""/>
+      <main className="landing">
+        <img className="linesbg linesbg--landing mt-5 pt-5" src="https://truemarket.ca/wp-content/themes/truemarket/assets/dist/images/bg-asset-lines-01.svg" alt="" />
+        <img className="pillbg pillbg--landing" src="https://truemarket.ca/wp-content/themes/truemarket/assets/dist/images/bg-asset-pill-right.svg" alt="" />
         <div className='container'>
-        <div class="hero">
-            <div class="hero__textbox">
-                <h3 class="subheading">We Build</h3>
-                <h1 class="hero__heading" data-sal="slide-left" data-sal-delay="700" data-sal-easing="easeInCubic">Performance Driven Websites</h1>
-                <p class="bodytext_hero">Elevate Your Digital Horizon</p>
-                <div data-sal="slide-left" data-sal-delay="900" data-sal-easing="easeInCubic">
-                    <a href="/new-websites/" class="button hero__button" target="_self">
-                Web Development              </a>
-                </div>
+          <div className="hero">
+            <div className="hero__textbox">
+              <h3 className="subheading">We Build</h3>
+              <h1 className="hero__heading" data-sal="slide-left" data-sal-delay="700" data-sal-easing="easeInCubic">
+                Performance Driven W{word.substring(0, index).padEnd(word.length, "\u00A0")}
+              </h1>
+              <p className="bodytext_hero">Elevate Your Digital Horizon</p>
+              <div data-sal="slide-left" data-sal-delay="900" data-sal-easing="easeInCubic">
+                <a href="/new-websites/" className="button hero__button" target="_self">
+                  Web Development
+                </a>
+              </div>
             </div>
-            <div class="hero__image" data-sal="slide-up" data-sal-delay="100" data-sal-easing="easeInCubic">
-                <img src="https://truemarket.ca/wp-content/themes/truemarket/assets/dist/images/hero-image.png" alt="Example of True Market built site" width="680" height="760" />
+            <div className="hero__image" data-sal="slide-up" data-sal-delay="100" data-sal-easing="easeInCubic">
+              <img src="https://truemarket.ca/wp-content/themes/truemarket/assets/dist/images/hero-image.png" alt="Example of True Market built site" width="680" height="760" />
             </div>
-        </div>
+          </div>
         </div>
         <div>
           <div className='container'>
             <div className='row'>
               <div className='col-12'>
-                <div className='px-lg-5'>
+                <div className=''>
                   <div className='customers_should_notice_box'>
                     <div className='text-center'>
                       <h2 className='customers_should_notice_h2'>
-                      Customers Should Notice
+                        Customers Should Notice
                       </h2>
                       <p className='amazing_business_p'>How amazing your business is</p>
                     </div>
@@ -40,9 +70,9 @@ const Hero = () => {
             </div>
           </div>
         </div>
-    </main>
+      </main>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
